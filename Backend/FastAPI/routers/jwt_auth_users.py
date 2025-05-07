77be0cf397f3194e5aeb3fd9d5e3ea8f3f32ecd0
ukashadforce <ukasha.dforce@gmail.com>
2025-05-07 21:08:35 +0500

    Update jwt_auth_users.py
    bug fix

--- Backend/FastAPI/routers/jwt_auth_users.py
+++ Backend/FastAPI/routers/jwt_auth_users.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 ALGORITHM = "HS256"
 ACCESS_TOKEN_DURATION = 1
 SECRET = "201d573bd7d1344d3a3bfce1550b69102fd11be3db6d379508b6cccc58ea230b"
-
+from passlib.context import CryptContext
 router = APIRouter(prefix="/jwtauth",
                    tags=["jwtauth"],
                    responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
